c5a2bcb93e80a41af9438d406009eef474162ebc
Daniel-Digid <daniel@dig.id>
2024-02-28 14:59:59 +0000

    d

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -26,11 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
 					init: function () {
 						gsap.set(content, { opacity: 0, y: 20 }); // Initial state
 						isTransitioning = true;
-						console.log(isTransitioning);
 						gsap.to(content, { opacity: 1, y: 0, duration: 0.3, stagger: 0.2 }); // Fade in on init
 						setTimeout(function() {
 							isTransitioning = false;
-							console.log(isTransitioning);
 						}, 3000);
 						setTimeout(function () {
 							gsap.to(content, { opacity: 0, y: -20, duration: 0.5 }); // Fade out
@@ -44,7 +42,6 @@ document.addEventListener("DOMContentLoaded", () => {
 						gsap.to(content, { opacity: 1, y: 0, duration: 0.3, stagger: 0.2 }); // Fade in after transition
 						setTimeout(function() {
 							isTransitioning = false;
-							console.log(isTransitioning);
 						}, 3000);
 						setTimeout(function () {
 							gsap.to(content, { opacity: 0, y: -20, duration: 0.5 }); // Fade out
@@ -55,7 +52,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		
 			document.querySelector(".swiper-button-next").addEventListener("click", function() {
 				if (!isTransitioning) {
-					console.log('next button click');
 					isTransitioning = true;
 					gsap.to(content, { opacity: 0, y: -20, duration: 0.3 }); // Fade out before transition
 					heroSwiper.autoplay.stop();
@@ -68,7 +64,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			});
 			document.querySelector(".swiper-button-prev").addEventListener("click", function() {
 				if (!isTransitioning) {
-					console.log('prev button click');
 					isTransitioning = true;
 					gsap.to(content, { opacity: 0, y: -20, duration: 0.3 }); // Fade out before transition
 					heroSwiper.autoplay.stop(); 
@@ -79,46 +74,6 @@ document.addEventListener("DOMContentLoaded", () => {
 					}, 1500); 
 				}
 			});
-		
-			
-
-
-
-		
-		
-		
-
-			/*function enableNavigation() {
-				heroSwiper.allowSlideNext = true;
-				heroSwiper.allowSlidePrev = true;
-			}
-
-			function disableNavigation() {
-				heroSwiper.allowSlideNext = false;
-				heroSwiper.allowSlidePrev = false;
-			}
-
-			heroSwiper.on('transitionStart', function () {
-				console.log( 'transstart' );
-				disableNavigation();
-				let content = gsap.utils.toArray([".hero-swiper-subtitle",".hero-swiper-title", ".hero-swiper-btn" ]);
-				gsap.fromTo(content, { y: '-20px', opacity: 0, autoAlpha: 0 }, { y: 0, opacity: 1, autoAlpha: 1, duration: 1, stagger: 0.2, onComplete: enableNavigation });
-
-			});
-
-			heroSwiper.on('transitionEnd', function () {
-				disableNavigation()
-				console.log( 'transend' );
-				let content = gsap.utils.toArray([".hero-swiper-subtitle",".hero-swiper-title", ".hero-swiper-btn" ]);
-				gsap.fromTo(content, { y: 0, opacity: 1, autoAlpha: 1, duration: 1, stagger: 0.2 }, { y: '-20px', opacity: 0, autoAlpha: 0, onComplete: enableNavigation  });
-			});*/
-
-			/*heroSwiper.on('slideChangeTransitionEnd', function () {
-				// Get the previous slide
-				let previousSlide = document.querySelector('.swiper-slide-prev');
-				// Create a GSAP animation
-				gsap.fromTo(previousSlide, { opacity: 1 }, { opacity: 0, duration: 1 });
-			});*/
 
 			var roomsSwiper = new Swiper(".roomsSwiper", {
 				loop: true,
