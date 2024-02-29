7e5c606b9a8160b4cc68680d5657ff9f70e0e212
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-02-29 13:28:06 +0000

    up

--- archive-zimmer.php
+++ archive-zimmer.php
@@ -3,5 +3,6 @@ get_header();
 do_action( 'before_main_content' );
 get_template_part( 'template-parts/archives/zimmer/intro' );
 get_template_part( 'template-parts/modules/loop-rooms' );
+get_template_part( 'template-parts/modules/loop-relaxation' );
 do_action( 'after_main_content' );
 get_footer();
