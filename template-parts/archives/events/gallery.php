6a615278bba45cb9dae8059914c061a1be7b8b7c
Daniel-Digid <daniel@dig.id>
2024-03-01 13:13:44 +0000

    d

--- template-parts/archives/events/gallery.php
+++ template-parts/archives/events/gallery.php
@@ -13,4 +13,19 @@
 		<div class="swiper-button-next"></div>
 		<div class="swiper-button-prev"></div>
 	</div>
+
+	<div class="swiper galleryHeroSwiperLow h-full block lg:hidden">
+		<div class="swiper-wrapper h-full">
+			<?php
+			$gallery = get_field( 'archives_events_gallery', 'options' );
+			if ( $gallery ) :
+				foreach ( $gallery as $image ) :
+					echo '<div class="swiper-slide h-full">' . wp_get_attachment_image( $image, 'image-square', false, array( 'class' => 'w-full max-w-full object-cover' ) ) . '</div>';
+				endforeach;
+			endif;
+			?>
+		</div>
+		<div class="swiper-button-next"></div>
+		<div class="swiper-button-prev"></div>
+	</div>
 </section>
