1e63333e348ea408ccb4b073d43ec7f1e2d405bf
pedro-digid <98165692+pedro-digid@users.noreply.github.com>
2024-03-04 19:31:54 +0000

    up

--- template-parts/components/card-rooms.php
+++ template-parts/components/card-rooms.php
@@ -2,7 +2,7 @@
 	<a href="<?php the_permalink(); ?>">
 		<?php
 		if ( has_post_thumbnail() ) :
-			the_post_thumbnail( 'full', array( 'class' => 'max-w-full object-cover mb-8' ) );
+			the_post_thumbnail( 'full', array( 'class' => 'max-w-full w-full object-cover mb-8' ) );
 		endif;
 		?>
 		<h2 class="title__card mb-4"><?php the_title(); ?></h2>
