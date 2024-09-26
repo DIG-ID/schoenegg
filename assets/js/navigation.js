7c4145df3d608685a8f8a6031b84e95132f11862
Daniel-Digid <daniel@dig.id>
2024-09-26 10:28:50 +0100

    d

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -4,39 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
   //wait until images, links, fonts, stylesheets, and js is loaded
   window.addEventListener("load", () => {
 
-    if ($(window).width() <= 1024) {
-      let buffer = 200; // change this to the amount of px you want the user to scroll before the sidebar moves
-
-      let lastScrollTop = 0; // to keep track of the last scroll position
-      
-      lenis.on("scroll", () => {
-      
-        let goesTo = lenis.animate.to;
-        let goesFrom = lenis.animate.from;
-        let current = lenis.animatedScroll;
-      
-        //console.log(current);
-      
-        if (goesFrom < buffer && goesTo >= buffer) {
-          // The user scrolled down from a position less than the buffer to a position greater than or equal to the buffer
-          $("#header-main").css('top', "-100%");
-        } else if (goesFrom >= buffer && goesTo < buffer) {
-          // The user scrolled up from a position greater than or equal to the buffer to a position less than the buffer
-          $("#header-main").css('top', "0");
-        } else if (goesTo < lastScrollTop) {
-          // The user scrolled up
-          $("#header-main").css('top', "0");
-        } else if (goesTo > lastScrollTop && goesTo > buffer) {
-          // The user scrolled down from a position greater than the buffer
-          $("#header-main").css('top', "-100%");
-        }
-      
-        // Update the last scroll position
-        lastScrollTop = goesTo;
-      
-      });
-      
+    if ($(window).width() <= 1024 && typeof lenis !== 'undefined') {
+        let buffer = 200;
+        let lastScrollTop = 0;
+    
+        lenis.on("scroll", () => {
+            let goesTo = lenis.animate.to;
+            let goesFrom = lenis.animate.from;
+            let current = lenis.animatedScroll;
+    
+            if (goesFrom < buffer && goesTo >= buffer) {
+                $("#header-main").css('top', "-100%");
+            } else if (goesFrom >= buffer && goesTo < buffer) {
+                $("#header-main").css('top', "0");
+            } else if (goesTo < lastScrollTop) {
+                $("#header-main").css('top', "0");
+            } else if (goesTo > lastScrollTop && goesTo > buffer) {
+                $("#header-main").css('top', "-100%");
+            }
+    
+            lastScrollTop = goesTo;
+        });
     }
+
     $(window).on('resize', function() {
       if ($(window).width() > 1024) {
         $("#header-main").css('top', "0");
