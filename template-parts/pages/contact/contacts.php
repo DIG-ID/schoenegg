96c16ac980bd74255c7231a14b3ff4aab05ae695
Daniel-Digid <daniel@dig.id>
2026-03-20 15:13:45 +0000

    Update contacts.php

--- template-parts/pages/contact/contacts.php
+++ template-parts/pages/contact/contacts.php
@@ -5,18 +5,28 @@
 				<h1 class="title__section"><?php the_field( 'contacts_title' ); ?></h1>
 			</div>
 			<div class="col-span-1 md:col-span-5">
-				<div class="text__area"><?php the_field( 'contacts_address' ); ?></div>
-				<div class="text__area"><p><?php esc_html_e( 'Tel: ', 'hs' ) ?><a class="!no-underline !text-[#737373]" href="tel:<?php the_field( 'contacts_phone' ); ?>"><?php the_field( 'contacts_phone' ); ?></a></p></div>
+				<?php if ( get_field( 'contacts_address' ) ) : ?>
+					<div class="text__area"><?php the_field( 'contacts_address' ); ?></div>
+				<?php endif; ?>
+				<?php if ( get_field( 'contacts_phone' ) ) : ?>
+					<div class="text__area"><p><?php esc_html_e( 'Tel: ', 'hs' ) ?><a class="!no-underline !text-[#737373]" href="tel:<?php the_field( 'contacts_phone' ); ?>"><?php the_field( 'contacts_phone' ); ?></a></p></div>
+				<?php endif; ?>
 				<?php
 				$whatsapp_number = get_field( 'footer_whatsapp', 'options' );
 				$wm = preg_replace( '/\s+/', '', $whatsapp_number );
+				if ( get_field( 'contacts_whatsapp' ) ) :
 				?>
-				<div class="text__area"><p><?php esc_html_e( 'Whatsapp: ', 'hs' ) ?><a class="!no-underline !text-[#737373]" href="https://wa.me/<?php echo $wm; ?>" target="_blank"><?php the_field( 'contacts_whatsapp' ); ?></a><br><?php esc_html_e( '(07:00 – 17:00 Uhr)', 'hs' ); ?></p></div>
+					<div class="text__area"><p><?php esc_html_e( 'Whatsapp: ', 'hs' ) ?><a class="!no-underline !text-[#737373]" href="https://wa.me/<?php echo esc_attr( $wm ); ?>" target="_blank"><?php the_field( 'contacts_whatsapp' ); ?></a><br><?php esc_html_e( '(07:00 – 17:00 Uhr)', 'hs' ); ?></p></div>
+				<?php endif; ?>
 				<?php if ( get_field( 'contacts_fax' ) ) : ?>
 					<div class="text__area"><p><?php esc_html_e( 'Fax: ', 'hs' ) ?><?php the_field( 'contacts_fax' ); ?></p></div>
 				<?php endif; ?>
-				<div class="text__area mb-8"><p><a class="!no-underline !text-[#737373]" href="mailto:<?php the_field( 'contacts_e-mail' ); ?>"><?php the_field( 'contacts_e-mail' ); ?></a></p></div>
-				<div class="text__area"><p><b><?php esc_html_e( 'Ihr Gastgeber', 'hs' ) ?></b><br><?php the_field( 'contacts_host_name' ); ?></p></div>
+				<?php if ( get_field( 'contacts_e-mail' ) ) : ?>
+					<div class="text__area mb-8"><p><a class="!no-underline !text-[#737373]" href="mailto:<?php the_field( 'contacts_e-mail' ); ?>"><?php the_field( 'contacts_e-mail' ); ?></a></p></div>
+				<?php endif; ?>
+				<?php if ( get_field( 'contacts_host_name' ) ) : ?>
+					<div class="text__area"><p><b><?php esc_html_e( 'Ihr Gastgeber', 'hs' ) ?></b><br><?php the_field( 'contacts_host_name' ); ?></p></div>
+				<?php endif; ?>
 			</div>
 			<div class="col-span-1 md:col-span-7">
 				<?php echo do_shortcode( get_field( 'contacts_form_shortcode' ) ); ?>
